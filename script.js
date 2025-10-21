6b39eab2ff6eb752dead0c22aef72f89739350cc
Artem Rivnyi <livegamedark@gmail.com>
2025-10-21 15:38:04 +0200

    Initial commit: Personal page with CI/CD

---
+++ script.js
@@ -0,0 +1,5 @@
+// Простой пример: обновляем год в футере
+document.addEventListener('DOMContentLoaded', function() {
+    // Можно добавить динамическое поведение, например:
+    // console.log("Page loaded!");
+});
